bcdd5de0d19413dff4def45cf9ef6b9a637da5fa
Anurag Singh <anurags@exzeo.com>
2017-08-18 22:14:06 +0530

    added camera support

--- app/src/main/java/click/tagit/MainActivity.java
+++ app/src/main/java/click/tagit/MainActivity.java
@@ -1,10 +1,15 @@
 package click.tagit;
 
+import android.Manifest.permission;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.FileProvider;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -15,10 +20,24 @@ import click.tagit.categorized.dummy.DummyContent.DummyItem;
 import click.tagit.grievance.GrievanceFragment.OnListGrievanceFragmentInteractionListener;
 import click.tagit.grievance.dummy.DummyContent;
 import click.tagit.uncategorized.UncategorizedFragment.OnListUncategorizedFragmentInteractionListener;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity implements
         OnListCategorizeFragmentInteractionListener, OnListGrievanceFragmentInteractionListener,
-        OnListUncategorizedFragmentInteractionListener {
+        OnListUncategorizedFragmentInteractionListener, EasyPermissions.PermissionCallbacks {
+
+    private static final int RC_CAMERA_PERM = 126;
+    private Uri mTempImageURI;
+    private String mTempImagePath;
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -44,6 +63,9 @@ public class MainActivity extends AppCompatActivity implements
                 case R.id.navigation_home:
                     return true;
                 case R.id.navigation_dashboard:
+                    mTempImageURI = null;
+                    mTempImagePath = null;
+                    checkCameraPermission();
                     return true;
                 case R.id.navigation_notifications:
                     return true;
@@ -112,5 +134,127 @@ public class MainActivity extends AppCompatActivity implements
     public void onListFragmentInteraction(
             click.tagit.uncategorized.dummy.DummyContent.DummyItem item) {
 
+    }
+
+    @AfterPermissionGranted(RC_CAMERA_PERM)
+    public void checkCameraPermission() {
+        Timber.d("checkCameraPermission() called");
+
+        String[] perms = {permission.CAMERA};
+        if (EasyPermissions.hasPermissions(MainActivity.this, perms)) {
+            Timber.d("checkCameraPermission() called: has permission");
+            // Have permissions, do the thing!
+            captureCamera();
+        } else {
+            Timber.d("checkCameraPermission() called: rationale permission");
+            // Ask for both permissions
+            EasyPermissions.requestPermissions(this,
+                    getString(R.string.rationale_camera_permission),
+                    RC_CAMERA_PERM, perms);
+        }
+    }
+
+    private void captureCamera() {
+        Timber.d("captureCamera() called");
+
+        File photoFile = null;
+        try {
+            photoFile = createImageFile();
+        } catch (IOException ex) {
+            // Error occurred while creating the File
+            Timber.d(ex, "captureCamera() called");
+            // TODO: show error or toast
+            return;
+        }
+        // Continue only if the File was successfully created
+        if (photoFile != null) {
+            Uri photoURI = null;
+            photoURI = FileProvider.getUriForFile(MainActivity.this, getPackageName()
+                    + getString(R.string.provider), photoFile);
+
+            mTempImageURI = photoURI;
+            mTempImagePath = photoFile.getAbsolutePath();
+            Timber.d("captureCamera() called: photoURI = %s", photoURI.toString());
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI); // set the image file name
+
+            // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+            // So as long as the result is not null, it's safe to use the intent.
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                // Start the image capture intent to take photo
+                startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+            }
+        } else {
+            // TODO: show error or toast
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        File storageDir = new File(getFilesDir(),
+                getString(R.string.dir_camera));
+
+        if (!storageDir.exists()) {
+            if (!storageDir.mkdirs()) {
+                Timber.e("createImageFile() called: error in creating dirs");
+                return null;
+            } else {
+                Timber.d("createImageFile() called: storageDir created");
+            }
+        } else {
+            Timber.d("createImageFile() called: storageDir already exists");
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
+        File capturedImage = new File(storageDir, timeStamp + ".jpeg");
+        if (!capturedImage.createNewFile()) {
+            Timber.e("createImageFile() called: error in creating capturedImage file");
+        } else {
+            Timber.d("createImageFile() called: capturedImage file created");
+        }
+        Timber.d("createImageFile() called: capturedImage file path is %s",
+                capturedImage.getPath());
+        return capturedImage;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Timber.d("onActivityResult() called with: requestCode = [" + requestCode
+                + "], resultCode = ["
+                + resultCode + "], data = [" + data + "]");
+
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Timber.d("onActivityResult: capture photo success");
+
+            } else { // Result was a failure
+                Timber.e("onActivityResult: capture photo failure");
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+            @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Timber.d("onRequestPermissionsResult() called with: requestCode = [" + requestCode
+                + "], permissions = [" + permissions + "], grantResults = [" + grantResults + "]");
+
+        // EasyPermissions handles the request result.
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults,
+                this);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+        Timber.d(
+                "onPermissionsGranted() called with: requestCode = [" + requestCode + "], perms = ["
+                        + perms + "]");
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+        Timber.d("onPermissionsDenied() called with: requestCode = [" + requestCode + "], perms = ["
+                + perms + "]");
     }
 }
